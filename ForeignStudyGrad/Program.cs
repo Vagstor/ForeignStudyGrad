9d9edae5e475b86f56d0b8ca0c85b22b2a11d8d5
Vagstor <avagt@inbox.ru>
2020-03-13 22:42:04 +0300

    соединение с бд, авторизация

--- ForeignStudyGrad/Program.cs
+++ ForeignStudyGrad/Program.cs
@@ -16,11 +16,17 @@ namespace ForeignStudyGrad
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IConfiguration CreateConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseConfiguration(CreateConfig());
     }
 }
--- ForeignStudyGrad/Startup.cs
+++ ForeignStudyGrad/Startup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ForeignStudyGrad.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace ForeignStudyGrad
 {
@@ -20,14 +24,35 @@ namespace ForeignStudyGrad
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            LinqToDB.Data.DataConnection.DefaultSettings = new MySettings
+            {
+                ConnString = Configuration.GetConnectionString("DefaultConnection")
+            };
+
+            //services.AddScoped<MainDb>();
+            services.AddScoped<SiteService>();
+            //services.AddScoped<HttpService>();
+            services.AddSingleton<IConfiguration>(Configuration);
+            //services.AddSingleton<MonitoringService>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new PathString("/Welcome/Login");
+                    options.AccessDeniedPath = new PathString("/Welcome/Login");
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -35,23 +60,28 @@ namespace ForeignStudyGrad
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Welcome/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            //MigrationRunner migrationRunner = new MigrationRunner();
+            var connStr = Configuration["ConnectionStrings:DefaultConnection"];
+            //migrationRunner.MigrationConnection(connStr);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            app.UseRouting();
+            app.UseCookiePolicy();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Welcome}/{action=Login}/{id?}");
             });
+
+            //app.ApplicationServices.GetService<MonitoringService>().Run();
         }
     }
 }
