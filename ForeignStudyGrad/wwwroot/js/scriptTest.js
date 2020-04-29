e59e7874f647f1924312334da3726652b817394b
ValeriLursa <kovaleva.valeri@mail.ru>
2020-04-29 14:45:26 +0300

    Добавлен файл Test1Theme. Добавлен файл scriptTest Убран вывод лекций в Theme

---
+++ ForeignStudyGrad/wwwroot/js/scriptTest.js
@@ -0,0 +1,18 @@
+﻿window.onload = init;
+function init() {
+    var words = document.getElementById("word");
+    for (var i = 0; i < words.length; i++) {
+        words[i].onclick = colorWords(words[i]);
+    }
+}
+var flag = 0;
+function colorWords(word) {
+    if (flag == 0) {
+        document.all.word.style.background = 'red';
+        flag = 1;
+    }
+    else {
+        document.all.word.style.background = 'white';
+        flag = 0;
+    }
+}
