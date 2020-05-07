deb13024be800f8b01f6e8eaedd574d621aed441
ValeriLursa <kovaleva.valeri@mail.ru>
2020-05-07 16:03:36 +0300

    Test1Theme: добавлены классы для проверки тестов, в задании 7 изменены imput на textarea scriptProverka: добавлена проверка заданий с открытым ответом

--- ForeignStudyGrad/wwwroot/js/scriptProverka.js
+++ ForeignStudyGrad/wwwroot/js/scriptProverka.js
@@ -1,5 +1,5 @@
 ﻿function proverka() {
-    console.log(clickSelect() + ' ' + clickProvColorWord() + ' ' + clickProvPrilSuch());
+    
     for (var i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
         tablinks[i].className = tablinks[i].className.replace(" w3-green", "");
@@ -14,4 +14,35 @@
     if (!clickProvPrilSuch()) {
         document.getElementById("srav").className += " w3-red";
     } else document.getElementById("srav").className += " w3-green";
+
+    sininomPred = document.getElementsByClassName("sininomPred");
+
+    if (!provMasClass(sininomPred)) {
+        document.getElementById("prov0").className += " w3-red";
+    } else document.getElementById("prov0").className += " w3-green";
+    question = document.getElementsByClassName("question");
+
+    if (!provMasClass(question)) {
+        document.getElementById("prov1").className += " w3-red";
+    } else document.getElementById("prov1").className += " w3-green";
+    nameText = document.getElementsByClassName("nameText");
+
+    if (!provMasClass(nameText)) {
+        document.getElementById("prov2").className += " w3-red";
+    } else document.getElementById("prov2").className += " w3-green";
+    textNew = document.getElementsByClassName("textNew");
+
+    if (!provMasClass(textNew)) {
+        document.getElementById("prov3").className += " w3-red";
+    } else document.getElementById("prov3").className += " w3-green";
+    console.log(clickSelect() + ' ' + clickProvColorWord() + ' ' + clickProvPrilSuch() + ' ' + provMasClass(sininomPred));
+}
+
+function provMasClass(array) {
+    var flag = true;
+    for (var i = 0; i < array.length; i++) {
+        console.log(array[i].value);
+        if (array[i].value == "") flag = false;
+    }
+    return flag;
 }
